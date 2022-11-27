c4c40c3988a94d713e5cbbf49e3ca205ce9e71be
Anurag Mishra <101906072+OPanurag@users.noreply.github.com>
2022-11-27 20:36:09 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -4,16 +4,19 @@ from instabot import *
 bot = Bot()
 
 # Credentials for insta account
-user = "test_py2002"
-passw = "testpy"
+user = "olauncherapp"
+passw = "tanujnotes123"
 
+# Login Command
 bot.login(username=user, password=passw)
 
-# Tile images
-img = spi.split('launch.jpeg', 6, 3)
+# Tile image information and command
+filename = 'img1'
+extension = '.jpeg'
+img = spi.split(filename + extension, 6, 3)
 
 # Upload loop
 for i in range(17, -1, -1):
-    img = "Images/launch_{}.jpeg".format(i)
-    bot.upload_photo(img, caption="test")
+    img = "Images/{}_{}.jpeg".format(filename, i)
+    bot.upload_photo(img, caption="____####____")
     i -= 1
